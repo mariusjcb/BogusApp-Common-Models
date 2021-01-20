991416c87df0646bfad95777d71cda7ba620e502
Marius Ilie <marius@iliemarius.ro>
2021-01-21 01:11:44 +0200

    package resolve

--- Package.swift
+++ Package.swift
@@ -12,18 +12,13 @@ let package = Package(
             targets: ["BogusApp-Common-Models"]
         ),
     ],
-    dependencies: [
-        .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "1.0.0"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.10"),
-        .package(url: "https://github.com/Realm/SwiftLint", from: "0.42.0"),
-        .package(url: "https://github.com/orta/Komondor", from: "1.0.6"),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BogusApp-Common-Models",
-            dependencies: [.product(name: "Danger", package: "danger-swift")]
+            dependencies: []
         ),
         .testTarget(
             name: "BogusApp-Common-ModelsTests",
@@ -31,21 +26,3 @@ let package = Package(
         ),
     ]
 )
-
-#if canImport(PackageConfig)
-    import PackageConfig
-
-    let config = PackageConfiguration([
-        "komondor": [
-            "pre-commit": [
-                "echo 'Running tests...'",
-                "swift test",
-                "echo 'Running SwiftFormat...'",
-                "swift run swiftformat .",
-                "echo 'Running SwiftLint...'",
-                "swift run swiftlint autocorrect --path Sources/",
-                "git add .",
-            ],
-        ],
-    ]).write()
-#endif
